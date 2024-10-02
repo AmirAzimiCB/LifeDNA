d6891daf9bd9c6a7a85b10be4edf0263cd8b85ad
Asraful-code235 <asrafulislamcodes@gmail.com>
2024-10-02 11:39:27 +0600

    added report images

---
+++ public/reports/index.ts
@@ -0,0 +1,269 @@
+import Sodium from "./Sodium Sensitivity.svg";
+import SaturatedFat from "./Saturated Fat Metabolism.svg";
+import GlutenSensitivity from "./Gluten Sensitivity.svg";
+import Lactose from "./Lactose Tolerance.svg";
+import Caffeine from "./Caffeine Sensitivity.svg";
+import Weight from "./Weight Change.svg";
+import Protein from "./Protein Intake.svg";
+import Peach from "./Peach Allergy.svg";
+import Peanut from "./Peanut Allergy.svg";
+import Shrimp from "./Shrimp Allergy.svg";
+import Sugar from "./Sugar Preference.svg";
+import Satiety from "./Satiety.svg";
+import Sensitivity from "./Sensitivity To Bitter Foods.svg";
+import Snacking from "./Snacking Behavior.svg";
+import Appetite from "./Appetite.svg";
+import Preference from "./Preference To Fatty Foods.svg";
+import Antioxidant from "./Antioxidant Status.svg";
+
+// Vitamins & Supplements
+
+import VitaminsAndSupplements from "./Vitamins & Supplements.svg";
+import VitaminC from "./Vitamin C Levels.svg";
+import VitaminE from "./Vitamin E Levels.svg";
+import Zinc from "./Zinc Levels.svg";
+import Iron from "./Iron Levels.svg";
+import Cholesterol from "./Cholesterol Levels.svg";
+import Copper from "./Copper Levels.svg";
+import DHA from "./DHA Levels.svg";
+import Liver from "./Liver Enzyme Levels.svg";
+import VitaminB6 from "./Vitamin B6 Keveks.svg";
+import VitaminB12 from "./Vitamin B12 Levels.svg";
+import VitaminB9 from "./Vitamin B9 (Folate Levels).svg";
+import CoQ10 from "./CoQ10 Levels.svg";
+import Probiotic from "./Probiotic Need.svg";
+import Magnesium from "./Magnesium Levels.svg";
+import Thyroid from "./Thyroid Function.svg";
+import Melatonin from "./Melatonin Levels.svg";
+import Calcium from "./Calcium Levels.svg";
+import VitaminD from "./Vitamin D Levels.svg";
+import Phosphorus from "./Phosphorus Levels.svg";
+import VitaminK from "./Vitamin K Levels.svg";
+import Biotin from "./Biotin Levels.svg";
+import VitaminB5 from "./Vitamin B5 Levels.svg";
+import Glomerular from "./Glomerular Filtration Rate.svg";
+import Proteinuria from "./Proteinuria.svg";
+import VisualAcuity from "./Visual Acuity.svg";
+import VitaminA from "./Vitamin A Levels.svg";
+
+// Fitness
+import Fitness from "./fitness.svg";
+import Muscle from "./Muscle Strength.svg";
+import Lactate from "./Lactate Clearancee.svg";
+import Power from "./Power Vs Endurance.svg";
+import Metabolic from "./Metabolic Rate And Metabolic Syndrome.svg";
+import Aerobic from "./Aerobic Capacity.svg";
+import BloodPressure from "./Blood Pressure & Exercise.svg";
+import HDL from "./HDL Cholesterol & Exercise.svg";
+import Insulin from "./Insulin Sensitivity & Exercise.svg";
+import Motivation from "./Motivation To Exercise.svg";
+import Exercise from "./Exercise Heart Rate Profile.svg";
+import Injury from "./Injury Predisposition.svg";
+import HandGrip from "./Hand Grip Strength.svg";
+import BoneDensity from "./Bone Density & Fracture Risk.svg";
+import Testosterone from "./Testosterone.svg";
+import PostExercise from "./Post-Exercise Recovery Rate.svg";
+import ExerciseInduced from "./Exercise-Induced Ischemia.svg";
+import Inflammation from "./Inflammation & Pain Tolerance.svg";
+import Circadian from "./Circadian Rhythm.svg";
+
+// Personality & Cognition
+import PersonalityAndCognition from "./Personality & Cognition.svg";
+import Agreeableness from "./Agreeableness.svg";
+import Conscientiousness from "./Conscientiousness.svg";
+import Openness from "./Openness.svg";
+import Extraversion from "./Extraversion.svg";
+import Neuroticism from "./Personality & Cognition.svg";
+import Loneliness from "./Loneliness.svg";
+import Resilience from "./Resilience.svg";
+import InstantGratification from "./Gratification.svg";
+import Impulsivity from "./Impulsivity.svg";
+import Fear from "./Fear.svg";
+import Positive from "./Positive.svg";
+import Harm from "./Harm.svg";
+import Hearing from "./Hearing.svg";
+import SensitivityToStimuli from "./Sensitivity.svg";
+import Educational from "./Educational.svg";
+import Hypnotizability from "./Hypnotizability.svg";
+import Creativity from "./Creativity.svg";
+import Optimism from "./Optimism.svg";
+import Emotional from "./Emotional Support.svg";
+import Depression from "./Depression.svg";
+import Maternal from "./Maternal.svg";
+import Gambling from "./Gambling.svg";
+import Mathematical from "./Mathematical.svg";
+import Intelligence from "./Intelligence.svg";
+import Musical from "./Musical.svg";
+import Aggressive from "./Aggressive.svg";
+import Risk from "./Risk.svg";
+
+// Wellness
+import Smoking from "./Smoking.svg";
+import Alcohol from "./Alcohol.svg";
+import Insomnia from "./Insomnia.svg";
+import Appendicitis from "./Appendicitis.svg";
+import Resting from "./Resting.svg";
+import AlcoholAddiction from "./Alcohol Addiction.svg";
+import Inguinal from "./Inguinal Hernia.svg";
+import Motion from "./Motion.svg";
+import Inflammatory from "./Inflammatory Response.svg";
+import Potential from "./Potential.svg";
+import DNA from "./DNA Damage & Detoxification Ability.svg";
+import Menstrual from "./Menstrual Cycle Length.svg";
+import Excessive from "./Excessive.svg";
+import Kidney from "./Kidney.svg";
+import Male from "./Male.svg";
+import Longevity from "./Longevity.svg";
+
+// Skincare
+import ExcessiveSkin from "./Excessive Skin Dryness.svg";
+import Acne from "./Acne.svg";
+import Facial from "./Facial Pigmental Spots.svg";
+import SkinTanning from "./Skin Tanning.svg";
+import Rosacea from "./Rosacea.svg";
+import Stretch from "./Stretch Marks.svg";
+import FacialWrinkles from "./Facial Wrinkles.svg";
+import Freckles from "./Freckles.svg";
+import Psoriasis from "./Psoriasis.svg";
+import Eczema from "./Eczema.svg";
+import Cellulite from "./Cellulite.svg";
+import Dandruff from "./Dandruff.svg";
+import SkinGlycation from "./Skin Glycation.svg";
+import Varicose from "./Varicose Veins.svg";
+import Contact from "./Contact Dermatitis.svg";
+
+export {
+  // nutrition
+  Sodium,
+  SaturatedFat,
+  GlutenSensitivity,
+  Lactose,
+  Caffeine,
+  Weight,
+  Protein,
+  Peach,
+  Peanut,
+  Sugar,
+  Shrimp,
+  Satiety,
+  Sensitivity,
+  Snacking,
+  Appetite,
+  Preference,
+  Antioxidant,
+
+  // Vitamins & Supplements
+  VitaminsAndSupplements,
+  VitaminC,
+  VitaminE,
+  Zinc,
+  Iron,
+  Cholesterol,
+  Copper,
+  DHA,
+  Liver,
+  VitaminB6,
+  VitaminB12,
+  VitaminB9,
+  CoQ10,
+  Probiotic,
+  Magnesium,
+  Thyroid,
+  Melatonin,
+  Calcium,
+  VitaminD,
+  Phosphorus,
+  VitaminK,
+  Biotin,
+  VitaminB5,
+  Glomerular,
+  Proteinuria,
+  VisualAcuity,
+  VitaminA,
+
+  // fitness
+  Fitness,
+  Muscle,
+  Lactate,
+  Power,
+  Metabolic,
+  Aerobic,
+  BloodPressure,
+  HDL,
+  Insulin,
+  Motivation,
+  Exercise,
+  Injury,
+  HandGrip,
+  BoneDensity,
+  Testosterone,
+  PostExercise,
+  ExerciseInduced,
+  Inflammation,
+  Circadian,
+
+  // Personality & Cognition
+  PersonalityAndCognition,
+  Agreeableness,
+  Conscientiousness,
+  Openness,
+  Extraversion,
+  Neuroticism,
+  Loneliness,
+  Resilience,
+  InstantGratification,
+  Fear,
+  Impulsivity,
+  Positive,
+  Harm,
+  Hearing,
+  SensitivityToStimuli,
+  Educational,
+  Hypnotizability,
+  Creativity,
+  Optimism,
+  Emotional,
+  Depression,
+  Maternal,
+  Gambling,
+  Mathematical,
+  Intelligence,
+  Musical,
+  Aggressive,
+  Risk,
+
+  // Wellness
+  Smoking,
+  Alcohol,
+  Insomnia,
+  Appendicitis,
+  Resting,
+  AlcoholAddiction,
+  Inguinal,
+  Motion,
+  Inflammatory,
+  Potential,
+  DNA,
+  Menstrual,
+  Excessive,
+  Kidney,
+  Male,
+  Longevity,
+
+  // Skincare
+  ExcessiveSkin,
+  Acne,
+  Facial,
+  SkinTanning,
+  Rosacea,
+  Stretch,
+  FacialWrinkles,
+  Freckles,
+  Psoriasis,
+  Eczema,
+  Cellulite,
+  Dandruff,
+  SkinGlycation,
+  Varicose,
+  Contact,
+};
